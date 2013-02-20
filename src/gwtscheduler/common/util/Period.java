164c8c45a84e5ce3d919373f6d7ea175d8bdee92
Mihail Lesikov <mlesikov@gmail.com>
2013-02-20 16:10:06 +0200

    added setters period class

--- src/gwtscheduler/common/util/Period.java
+++ src/gwtscheduler/common/util/Period.java
@@ -41,4 +41,12 @@ public class Period {
   public int getDays() {
     return CalendarUtil.getDaysBetween(start.asDate(),end.asDate());
   }
+
+  public void setStart(DateTime start) {
+    this.start = start;
+  }
+
+  public void setEnd(DateTime end) {
+    this.end = end;
+  }
 }
